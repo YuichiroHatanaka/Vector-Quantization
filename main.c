d31204889e0b20a5f2d5945cba130dfc4fb7da15
YuichiroHatanaka <yuichirohata1121@gmail.com>
2024-01-13 21:11:21 +0900

    Add code

--- main.c
+++ main.c
@@ -5,14 +5,20 @@
 #include"codebook.h"
 
 int main(void) {
-	char filename[] = "sample.pgm";		//ベクトル量子化したいファイルを選択
-	char cbfilename[] = "codebook.dat";	//量子化するときの代表ベクトルを格納するコードブック
-	char vqfilename[] = "vq.dat";		//ベクトル量子化されたファイルのデータ
+	char filename[] = "Data/sample.pgm";		//ベクトル量子化したいファイルを選択
+	char cbfilename[] = "Data/codebook.dat";	//量子化するときの代表ベクトルを格納するコードブック
+	char tmpfilename[] = "Data/tmp.dat";
+	char vqfilename[] = "Data/vqmap.dat";		//ベクトル量子化されたファイルのデータ
+	
+//	char filename[] = "sample.pgm";		//ベクトル量子化したいファイルを選択
+//	char cbfilename[] = "codebook.dat";	//量子化するときの代表ベクトルを格納するコードブック
+//	char vqfilename[] = "vq.dat";		//ベクトル量子化されたファイルのデータ
 
-	char row = 1;				//ベクトル化するとき取得する行の要素(未対応なため１で固定)
+	char row = 2;				//ベクトル化するとき取得する行の要素(未対応なため１で固定)
 	char column = 2;			//ベクトル化をするとき取得する列の要素
 	int width, height;			
 	int rowNum, columnNum;
+	int start = 13;
 
 	checkP5(filename);
 
@@ -23,6 +29,6 @@ int main(void) {
 
 	numVec(width, height, row, column, &rowNum, &columnNum);
 	printf("number of rows: %d\nnumber of columns: %d\n\n", rowNum, columnNum);
-	codebook(filename, cbfilename, vqfilename, row, column, rowNum, columnNum);
+	codebook(row, column, rowNum, columnNum, width, start, filename, cbfilename, tmpfilename, vqfilename);
 	return 0;
 }
